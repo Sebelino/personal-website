315168ccf1bf7f9ecde9f42dd3aa6071704ee942
Sebastian Olsson <sebelino7@gmail.com>
2016-07-16 13:05:51 +0200

    Started working on an app for displaying the feed.

--- app/controllers/feed.go
+++ app/controllers/feed.go
@@ -1,12 +1,16 @@
-package main
+package controllers
 
 import (
     "fmt"
+    "github.com/revel/revel"
     "github.com/mmcdole/gofeed"
 )
 
-func main() {
-    fmt.Println("hello")
+type Feed struct {
+	*revel.Controller
+}
+
+func (c Feed) Atom() revel.Result {
     fp := gofeed.NewParser()
     feed, _ := fp.ParseURL("https://joindiaspora.com/public/sebelino.atom")
     items := feed.Items
@@ -14,4 +18,5 @@ func main() {
         fmt.Println(item.Title)
     }
     fmt.Println(feed.Title)
+	return c.Render()
 }
