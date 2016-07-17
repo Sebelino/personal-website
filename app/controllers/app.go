596d8e7f19bb04821826ef97c5a2d552120ad2ff
Sebastian Olsson <sebelino7@gmail.com>
2016-07-17 23:24:35 +0200

    Nicer date formatting.

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -3,7 +3,18 @@ package controllers
 import (
     "github.com/revel/revel"
     "github.com/mmcdole/gofeed"
+    "time"
 )
+
+func init() {
+    /* 2016-07-17T22:12:42Z -> 2016-07-17 22:12:42 */
+    revel.TemplateFuncs["datetimefmt"] = func(timestamp string) string {
+        loc, _ := time.LoadLocation("Europe/Stockholm")
+        t1, _ := time.ParseInLocation(time.RFC3339, timestamp, loc)
+        fmtted := t1.Format(time.RFC1123)
+        return fmtted
+    }
+}
 
 type App struct {
 	*revel.Controller
